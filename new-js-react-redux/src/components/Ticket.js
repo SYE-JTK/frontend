fb3061eaff0390b531cf92b5882bc4e0de7df23c
jbpeek <jbpeek16@stlawu.edu>
2019-09-22 16:15:15 -0400

    Adds some styles

--- new-js-react-redux/src/components/Ticket.js
+++ new-js-react-redux/src/components/Ticket.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './ticketHomePage.css';
+import './page_layout/page.css';
 
 import { connect } from "react-redux";
 import { completeToDo, updateTicket } from "../actions";
@@ -29,18 +30,8 @@ class Ticket extends Component {
   };
 
   classes = (status) => {
-    switch(status){
-      case 'Pending':
-        return 'red';
-      case 'Started':
-        return 'yellow';
-      case 'In Review':
-        return 'yellow';
-      case 'Done':
-        return 'green';
-      default:
-        return 'red';
-    }
+    return status === 'Done' ? 'color-bg-success' : 
+    status === 'Pending' ? 'color-bg-pending' : 'color-bg-main';
   };
 
   button = (status) => {
@@ -61,18 +52,25 @@ class Ticket extends Component {
   render() {
     const { ticketId, title, description, owner, status } = this.props;
     return (
-      <div className={`single-ticket-container ticket-row-${this.classes(status)}`}>
+      <div className={`single-ticket-container ${this.classes(status)}`}>
         <div className='ticket-text-container'>
           
+          <div className='ticket-text-spacer ticket-text-header'>Title</div>
           <div className='ticket-text-spacer'>{ title }</div>
-          <div className='ticket-text-spacer'>Ticket Description</div>
-          <div className='ticket-text-spacer'>{ description }</div>
+          <div className='ticket-text-spacer ticket-text-header'>Description</div>
+          {description ? <div className='ticket-text-spacer'>{ description }</div> : null}
+          {owner? <div className='ticket-text-spacer ticket-text-header'>Owner</div> : null}
           <div className='ticket-text-spacer'>{ owner }</div>
-          <div className='ticket-text-spacer'>Status: { status }</div>
 
-          <button onClick={() => this.handleCompleteClick(ticketId, status)}>
-            { this.button(status) }
-          </button>
+          <div className='button-container'>
+            <button
+              className='button-main'
+              onClick={() => this.handleCompleteClick(ticketId, status)}
+            >
+              { this.button(status) }
+            </button>
+          </div>
+          
 
         </div>
       </div>
