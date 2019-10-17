f07b533890da91c213d33dec95e3d4d33f46a05a
jbpeek <jbpeek16@stlawu.edu>
2019-10-17 11:37:44 -0400

    File clean-up, gets rid of the nested app and puts everything in frontend.

--- src/actions/simpleAction.ts
+++
@@ -1,9 +0,0 @@
-/*
- src/actions/simpleAction.js
-*/
-export const simpleAction = () => (dispatch: (arg0: { payload: string; type: string; }) => void) => {
- dispatch({
-  payload: 'result_of_simple_action',
-  type: 'SIMPLE_ACTION'
- })
-}
