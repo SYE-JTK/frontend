73348239dde7a9b36cf240a4306dedf231f61a4f
jbpeek <jbpeek16@stlawu.edu>
2019-10-31 20:28:50 -0400

    style changes

--- src/components/note/NoteTaking.js
+++ src/components/note/NoteTaking.js
@@ -1,6 +1,8 @@
 // eslint-disable-next-line
 import React, { Component } from "react";
 
+import '../ticketHomePage.css';
+import '../page_layout/page.css';
 
 
 
@@ -20,7 +22,7 @@ class NoteTaking extends React.Component {
   addNote(event){
     console.log("working");
 
-    if(this.theTitle.value != ""){
+    if(this.theTitle.value !== ""){
       var newItem = {
         title: this.theTitle.value,
         note: this.theNote.value
@@ -48,7 +50,7 @@ class NoteTaking extends React.Component {
         <header>
            <h1> Notes </h1>
         </header>
-        <div className = "main-content">
+        <div className = "main-content margin-b-3">
           <u1>
             {this.state.items.map( (val) => <li>{val.title}-{val.note}</li>)}
           </u1>
@@ -56,15 +58,17 @@ class NoteTaking extends React.Component {
         <footer>
           <form onSubmit = {this.addNote}> 
             <input
-             type="text"
-             placeholder = "Enter note title here"
-             ref = {(title) => this.theTitle = title}
-             />
-            <textarea 
-            placeholder = "Enter note here"
-            ref = {(note) => this.theNote = note}
-            />
-            <button type="submit">AddNote</button>
+              className='input-main margin-b-2'
+              type="text"
+              placeholder = "Enter note title here"
+              ref = {(title) => this.theTitle = title}
+            /><br/>
+            <textarea
+              className='textarea-main margin-b-2' 
+              placeholder = "Enter note here"
+              ref = {(note) => this.theNote = note}
+            /><br/>
+            <button className='button-main' type="submit">Add Note</button>
           </form>
         </footer>
       </div> 
