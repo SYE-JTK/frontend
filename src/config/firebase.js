dfd0275650d5a0be6f6570a44d97e1a637a7a6aa
jbpeek <jbpeek16@stlawu.edu>
2020-01-28 09:35:23 -0500

    Firebase hosting and changes to enable PWA

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,12 +1,12 @@
 import * as firebase from "firebase";
 
-import { FirebaseConfig } from "./keys";
+import { FirebaseConfig } from "./dev";
 
 if (!firebase.apps.length) {
     firebase.initializeApp(FirebaseConfig);
 }
 
-const databaseRef = firebase.database().ref();
+const databaseRef = firebase.database().ref(); 
 
 export const ticketsRef = databaseRef.child("tickets");
 
