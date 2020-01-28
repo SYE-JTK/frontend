54a98de88fa1c2f39915bb914c381fbc4acb65d6
Kira <kiramurphy13@yahoo.com>
2020-01-28 16:13:04 -0500

    news feed avatar

--- src/components/posts/Posts.js
+++ src/components/posts/Posts.js
@@ -3,6 +3,8 @@ import React from "react";
 
 import _ from "lodash";
 
+import Avatar from '@material-ui/core/Avatar';
+
 import { connect } from "react-redux";
 
 import * as actions from "../../actions/notesActions";
@@ -11,29 +13,42 @@ import store from '../../store';
 
 class Posts extends React.Component {
 
+
+
   renderPosts() {
+    
     const { posts } = this.props;
     return (
       _.map(posts, (value, key) => {
         if (value.owner === store.getState().session.currentUser) {
+          let avatarTitle = value.ownerName.split(/\s/).reduce((response,word)=> response+=word.slice(0,1),'')
           return (
             <div key={ key }>
-            <div className='note-owner'>You</div>
+            <div className ="row">
+            <Avatar className="mt-2">{avatarTitle}</Avatar>
               <div className='note-container'>
                 <div className='note-title'>{ value.title }</div>
                 <div className='note-content'>{ value.content }</div>
+              </div>
               </div>
               <br/>
               <br/>
             </div>
           )
         } else{
+            let avatarTitle = "und"
+            if (value.ownerName){
+              avatarTitle = value.ownerName.split(/\s/).reduce((response,word)=> response+=word.slice(0,1),'')
+            }
+           
             return (
                 <div key={ key }>
-                <div className='note-owner'>{ value.ownerName }</div>
+                <div className="row">
+                <Avatar className="mt-2">{avatarTitle}</Avatar>
                   <div className='note-container'>
                     <div className='note-title'>{ value.title }</div>
                     <div className='note-content'>{ value.content }</div>
+                  </div>
                   </div>
                   <br/>
                   <br/>
